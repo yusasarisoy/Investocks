44ad944726268ab6e82908996bfe63e8d725bf38
Yuşa Sarısoy <yusasarisoy@icloud.com>
2022-01-29 11:48:18 +0300

    Initial Commit

---
+++ Investocks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Investocks
+//
+//  Created by Yuşa on 29.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
