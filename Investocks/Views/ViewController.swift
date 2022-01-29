7b986a6d54daae0ee903fc1ab402b6124cac374a
Yuşa Sarısoy <yusasarisoy@icloud.com>
2022-01-29 12:26:53 +0300

    The auto-layout library called SnapKit was added.

--- Investocks/Views/ViewController.swift
+++ Investocks/Views/ViewController.swift
@@ -5,15 +5,36 @@
 //  Created by Yuşa on 29.01.2022.
 //
 
+import SnapKit
 import UIKit
 
 class ViewController: UIViewController {
 
+  // MARK: - Properties
+
+  /// Displays the text "Hello, world!" to welcome users.
+  private lazy var labelHello: UILabel = {
+    let label = UILabel()
+    label.text = "Hello, world!"
+    view.addSubview(label)
+
+    return label
+  }()
+
+  // MARK: - Lifecycle
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+
+    setupConstraints()
   }
 
+  // MARK: - Helpers
 
+  /// Allows to setup constraints of **UIView** elements using SnapKit.
+  private func setupConstraints() {
+    labelHello.snp.makeConstraints { make in
+      make.centerX.centerY.equalToSuperview()
+    }
+  }
 }
-
